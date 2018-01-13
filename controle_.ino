10dcea401910e627edf0e9e523845caeade9e9df
jorge1752 <35276733+jorge1752@users.noreply.github.com>
2018-01-13 16:46:00 -0200

    upload da programação
    vai demorar pra terminar

--- controle_.ino
+++ controle_.ino
@@ -1,4 +1,4 @@
-// Módulo transmissor utilizando o FS1000A.
+ // Módulo transmissor utilizando o FS1000A.
 
 #include <RH_ASK.h>
 #include <SPI.h>
@@ -23,20 +23,29 @@ void setup() {
 
 void loop() {
   int cliente = 1;
-  int porta = 8;
+  int cliente2 = 2;
+  int porta1 = 8;
+  int porta2 = 9;
   int estado = digitalRead(2);
-  int sensorY = analogRead(A0);
-  int sensorX = analogRead(A1);
+  int estado2 = digitalRead(3);
+  int sensorY2 = analogRead(A2);
+  int sensorX2 = analogRead(A3);
+  int sensorY1 = analogRead(A0);
+  int sensorX1 = analogRead(A1);
   // print out the value you read:
-  Serial.println(sensorY);
-  Serial.println(sensorX);
+  Serial.println(sensorY1);
+  Serial.println(sensorX1);
+  Serial.println(sensorY2);
+  Serial.println(sensorX2);
   delay(1000);
-  sprintf(msg, "%d%d%d", cliente, porta, estado);
+  sprintf(msg, "%d%d%d", cliente, porta1, estado);
 
-  sprintf(debug, "Cliente: %d, Porta: %d, Estado: %d", cliente, porta, estado);
+  sprintf(debug, "Cliente1: %d, Porta1: %d, Estado: %d", cliente, porta1, estado);
+  sprintf(debug, "Cliente2: %d, Porta2: %d, Estado2: %d", cliente2, porta2, estado2);
   Serial.println(debug);
 
   driver.send((uint8_t *)msg, strlen(msg));
   driver.waitPacketSent();
   count++;
 }
+
