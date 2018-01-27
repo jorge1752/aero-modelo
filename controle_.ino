9e5c8c77f6f9ba3d171c1f3e6765f1b298849b99
jorge1752 <35276733+jorge1752@users.noreply.github.com>
2018-01-27 17:34:59 -0200

    Programação do controle
    A programação do radio controle foi modificada,adicionando a codificação da radio frequência e a potencia da radio frequência.

--- controle_.ino
+++ controle_.ino
@@ -1,51 +1,52 @@
- // Módulo transmissor utilizando o FS1000A.
+// Módulo transmissor utilizando o FS1000A.
 
-#include <RH_ASK.h>
+// Comandos:
+//  joystick esquerdo:
+//    frente/trás: 
+//    esquerda/direita:
+//    botão:
+//  joystick direito:
+//    frente/trás: 
+//    esquerda/direita:
+//    botão:
+
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
 #include <SPI.h>
 
-// Transmissão utilizando o pino D4.
-RH_ASK driver(2000, 0, 4);
-
-int count = 0;
-char msg[RH_ASK_MAX_MESSAGE_LEN];
-
-char debug[100];
+RF24 radio(7, 10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int dataToSend[6];
 
 void setup() {
   Serial.begin(9600);
   Serial.print("Iniciando...");
-  if (!driver.init())
-    Serial.println(" Erro!");
-  else
-    Serial.println("Ok!");
+
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.setChannel(108); //108 - 2.508 Ghz //0-124 2.4gHz-2.5gHz
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.openWritingPipe(pipe);
+  
   pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
 }
 
 void loop() {
-  int cliente = 1;
-  int cliente2 = 2;
-  int porta1 = 8;
-  int porta2 = 9;
   int estado = digitalRead(2);
   int estado2 = digitalRead(3);
   int sensorY2 = analogRead(A2);
   int sensorX2 = analogRead(A3);
   int sensorY1 = analogRead(A0);
   int sensorX1 = analogRead(A1);
-  // print out the value you read:
-  Serial.println(sensorY1);
-  Serial.println(sensorX1);
-  Serial.println(sensorY2);
-  Serial.println(sensorX2);
-  delay(1000);
-  sprintf(msg, "%d%d%d", cliente, porta1, estado);
 
-  sprintf(debug, "Cliente1: %d, Porta1: %d, Estado: %d", cliente, porta1, estado);
-  sprintf(debug, "Cliente2: %d, Porta2: %d, Estado2: %d", cliente2, porta2, estado2);
-  Serial.println(debug);
-
-  driver.send((uint8_t *)msg, strlen(msg));
-  driver.waitPacketSent();
-  count++;
+  dataToSend[0] = estado;     
+  dataToSend[1] = estado2;    
+  dataToSend[2] = sensorY2;     
+  dataToSend[3] = sensorX2;    
+  dataToSend[4] = sensorY1;    
+  dataToSend[5] = sensorX1;
+  radio.write(dataToSend, sizeof(dataToSend));
 }
-
